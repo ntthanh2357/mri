d544fb8c39e425cbd4106edabb06049399f5acc5
Dinh Huy Hoang <hoangdhde180623@fpt.edu.vn>
2026-06-05 13:13:45 +0700

    cập nhật tự động bắt ip wifi

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  // Thay thế bằng IP Wifi của máy tính bạn (hiện tại là 192.168.1.2) để điện thoại thật có thể kết nối được
-  API_URL: 'http://192.168.1.2:3000',
+  // Tự động cập nhật bởi script update-ip.js
+  API_URL: 'http://172.20.10.4:3000',
   APP_NAME: 'NeuroScan AI',
 };
 
